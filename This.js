cb79bc2a56a423ea88103d468a029ef110e49622
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-08 21:58:03 +0600

    Javascript Advanced This Keyword

---
+++ This.js
@@ -0,0 +1,42 @@
+// This a javascript keyword
+// it mainly follow four rules:
+// global , object , clear , new keyword rules
+// use strict used to avoid bad practice.
+
+//1. object rules
+const person = {
+  name: "faisal",
+  age: 12,
+  msg: function () {
+    console.log("My Name is : ", this.name); // this means object
+  },
+};
+person.msg();
+
+//2. Global means window
+function hello() {
+  console.log("Global value is : ", this);
+}
+hello();
+
+//3. clear rules
+const student1 = {
+  name: "faisal rahman",
+  id: 1,
+  address: "Dhaka",
+  student2: {
+    id: 2,
+    name: "sakibul islam faisal",
+    value: function () {
+      console.log(this);
+    },
+  },
+};
+
+student1.student2.value(); // call the student2 object => value
+
+// if we want to custom called the object student1
+student1.student2.value.call(student1);
+
+// called normal function it means global object
+// except custom object all the object are global
