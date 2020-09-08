bef612cc129d9abcd6cee91b6f264364e431595d
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-08 19:22:06 +0600

    Javacript Operator & Data Type

---
+++ Operator.js
@@ -0,0 +1,57 @@
+//Javascript Different Types Datatype - String , Number , boolean
+
+//1. Arithmetic Operator in javascript
+const number1 = 100;
+const number2 = 20;
+
+// Add operator
+const sum = number1 + number2; // ( + sign is the operator)
+console.log("Add value is : ", sum);
+//Here number1 & number2 is an operand . Similar all.
+
+//Subtraction Operator
+const sub = number1 - number2;
+console.log("Subtraction value is : ", sub); // ( - sign is the operator)
+
+//Multiplication Operator
+const mul = number1 * number2; // ( * sign is the operator)
+console.log("Multiplication value is : ", mul);
+
+//Division Operator
+const division = number1 / number2; // ( / sign is the operator)
+console.log("Division value is : ", division);
+
+//Modulus operator
+const mod = number1 % number2; // ( % sign is the operator)
+console.log("Modulus value is : ", mod);
+
+//Increment operator
+var a = 10;
+a++; // Post Increment operator and this line value is 10
+console.log("Post Increment value is : ", a); // this line value is 11
+
+//Pre Increment Operator
+var b = 11;
+++b; // Pre Increment Operator and this line value is 12
+console.log("Pre Increment value is : ", b);
+
+//2. Compare Operator
+var p = 50;
+var r = 20;
+
+//operator
+p == r; // equal operator
+p != r; // not equal operator
+p > r; // greater then operator
+p < r; // less than operator
+p >= r; // greater than equal operator
+p <= r; // less than equal operator
+
+//3. Logical Operator
+p && r; // And operator
+p || r; // Or operator
+
+//4. Assignment Operator
+p += r;
+p -= r;
+p *= r;
