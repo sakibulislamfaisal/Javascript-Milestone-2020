e12b34ae47df4df1c37787aee03040182a2b481b
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-09 20:11:30 +0600

    Javascript ES6  Rest Parameter

---
+++ RestParameter.js
@@ -0,0 +1,26 @@
+//Rest Parameter => arguments
+// arguments access all the value if you do not pass in the function
+function Add(a, b, c, d) {
+  const sum = a + b + c + d;
+  console.log(sum, arguments);
+}
+add(2, 3);
+Add(2, 3, 4, 6, 6, 8, 9);
+
+// //using arguments in ES5
+function add() {
+  for (i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+add("faisal", "rahman");
+
+// In ES6 it is not working
+// const add = () => {
+//   for (i = 0; i < arguments.length; i++) {
+//     console.log(arguments[i]);
+//   }
+// };
+// add("faisal", "rahman");
+
+// why it is error => it is called an array like object that means similar type of array but it is object.
