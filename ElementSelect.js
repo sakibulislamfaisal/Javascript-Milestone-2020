6a032e7d4b243b01a323e3ac01a96fd74ddd4920
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-09 23:08:50 +0600

    Javascript Element Select Method

---
+++ ElementSelect.js
@@ -0,0 +1,7 @@
+// for id select
+document.getElementById("Id Name");
+// for class select
+document.querySelectorAll();
+document.getElementsByClassName();
+// for tag select
+document.getElementsByTagName();
