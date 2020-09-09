1ee38685794fe0a7c82da5cf2164290aba87b3f7
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-09 21:55:23 +0600

    Javascript Basic Console

---
+++ Console.js
@@ -0,0 +1,17 @@
+//Console related logic
+//console.dir(console);
+
+//testing purpose
+console.assert(2 == 2, "Error");
+//console.clear();
+const name = "faisal";
+console.count(name);
+console.count(name);
+console.log(
+  "%c Foyshal Rahman",
+  "font-size: 20px ,color:red, border-radius:10px"
+);
+const arr = ["faisal", "rahman", "sakib"];
+console.table(arr);
+console.info("Info is called");
+console.warn("warn is called");
